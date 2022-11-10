77f4f627edd68c655f3e15043cdfb1741aa7698e
Yohann Assouline <yohannassouline@hotmail.fr>
2022-11-10 02:51:00 +0100

    Remove functions from mod.rs and add them in new preset/getters.rs

--- src/preset/mod.rs
+++ src/preset/mod.rs
@@ -7,6 +7,7 @@ use std::error::Error;
 
 use std::{fs, path};
 
+mod getters;
 mod print;
 mod run;
 mod verify;
@@ -114,44 +115,6 @@ impl Preset {
             .await?;
         }
         Ok(())
-    }
-
-    pub fn get_service_name_from_uuid(&self, uuid: &str) -> Option<String> {
-        let Some(services) = &self.services else {
-            return None;
-        };
-
-        for (ser_name, ser_data) in services.iter() {
-            if ser_data.uuid == uuid {
-                return Some(ser_name.to_owned());
-            }
-        }
-        None
-    }
-
-    pub fn get_characteristic_name_from_uuid(
-        &self,
-        ser_uuid: &str,
-        char_uuid: &str,
-    ) -> Option<String> {
-        let Some(services) = &self.services else {
-            return None;
-        };
-
-        for (_, ser_data) in services.iter() {
-            if ser_data.uuid == ser_uuid {
-                let Some(characteristics) = &ser_data.characteristics else {
-                    return None;
-                };
-
-                for (char_name, char_data) in characteristics.iter() {
-                    if char_data.uuid == char_uuid {
-                        return Some(char_name.to_owned());
-                    }
-                }
-            }
-        }
-        None
     }
 }
 
